720725bf0e3a255d7a9990f9d0147f035b09e431
Rob Whelan <hallwayraptor@gmail.com>
2023-06-19 02:41:41 -0300

    add slider and hold to change number function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function App() {
 	//Random Number
 	const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
 	const decrement = () => {
-		const currentGuess = guess === "" ? 0 : Number(guess);
-		if (currentGuess > 1) {
-			setGuess(currentGuess - 1);
-		}
+		setGuess((currentGuess) => {
+			if (currentGuess > 1) {
+				return Number(currentGuess) - 1;
+			} else {
+				return currentGuess;
+			}
+		});
 	};
 	const increment = () => {
-		const currentGuess = guess === "" ? 0 : Number(guess);
-		if (currentGuess < 100) {
-			setGuess(currentGuess + 1);
-		}
+		setGuess((currentGuess) => {
+			if (currentGuess < 100) {
+				return Number(currentGuess) + 1;
+			} else {
+				return currentGuess;
+			}
+		});
 	};
 	// state variables
 	const [targetNumber, setTargetNumber] = useState(generateRandomNumber);
@@ -24,8 +30,12 @@ export default function App() {
 
 	// handlers
 	const handleChange = (event) => {
-		const newGuess = event.target.value;
+		const newGuess = Number(event.target.value);
 		setGuess(newGuess > 100 ? 100 : newGuess);
+	};
+
+	const handleSliderChange = (event) => {
+		setGuess(Number(event.target.value));
 	};
 
 	const handleReset = () => {
@@ -67,6 +77,7 @@ export default function App() {
 				gameOver={gameOver}
 				increment={increment}
 				decrement={decrement}
+				handleSliderChange={handleSliderChange}
 			/>
 			<GameMessage
 				guessCount={guessCount}
@@ -87,36 +98,89 @@ const GameFormArea = ({
 	gameOver,
 	increment,
 	decrement,
-	handleDecrementMouseDown,
-	handleIncrementMouseDown,
-	handleMouseUp,
-}) => (
-	<form onSubmit={onSubmit}>
-		<div className="number-input-container">
-			<button type="button" onClick={decrement} disabled={gameOver}>
-				-
-			</button>
-			<input
-				type="number"
-				value={guess}
-				onChange={onChange}
-				min="1"
-				max="100"
-				className="no-spin"
-			/>
-			<button type="button" onClick={increment} disabled={gameOver}>
-				+
-			</button>
-		</div>
-		{gameOver ? (
-			<button type="button" onClick={onReset}>
-				Reset
-			</button>
-		) : (
-			<button type="submit">Guess</button>
-		)}
-	</form>
-);
+	handleSliderChange,
+}) => {
+	const [isIncrementing, setIsIncrementing] = useState(false);
+	const [isDecrementing, setIsDecrementing] = useState(false);
+
+	const incrementInterval = useRef();
+	const decrementInterval = useRef();
+
+	const handleMouseDown = (direction) => {
+		if (direction === "increment") {
+			increment();
+			setIsIncrementing(true);
+		} else {
+			decrement();
+			setIsDecrementing(true);
+		}
+	};
+
+	useEffect(() => {
+		if (isIncrementing) {
+			incrementInterval.current = setInterval(increment, 200);
+		}
+		return () => clearInterval(incrementInterval.current);
+	}, [isIncrementing, increment]);
+
+	useEffect(() => {
+		if (isDecrementing) {
+			decrementInterval.current = setInterval(decrement, 200);
+		}
+		return () => clearInterval(decrementInterval.current);
+	}, [isDecrementing, decrement]);
+
+	return (
+		<form onSubmit={onSubmit}>
+			<div className="input-slider-container">
+				<div className="slider-container">
+					<input
+						type="range"
+						min="1"
+						max="100"
+						value={guess}
+						onChange={handleSliderChange}
+					/>
+				</div>
+				<div className="number-input-container">
+					<button
+						type="button"
+						onMouseDown={() => handleMouseDown("decrement")}
+						onMouseUp={() => setIsDecrementing(false)}
+						onMouseLeave={() => setIsDecrementing(false)}
+						disabled={gameOver}
+					>
+						-
+					</button>
+					<input
+						type="number"
+						value={guess}
+						onChange={onChange}
+						min="1"
+						max="100"
+					/>
+
+					<button
+						type="button"
+						onMouseDown={() => handleMouseDown("increment")}
+						onMouseUp={() => setIsIncrementing(false)}
+						onMouseLeave={() => setIsIncrementing(false)}
+						disabled={gameOver}
+					>
+						+
+					</button>
+				</div>
+			</div>
+			{gameOver ? (
+				<button type="button" onClick={onReset}>
+					Reset
+				</button>
+			) : (
+				<button type="submit">Guess</button>
+			)}
+		</form>
+	);
+};
 const GameMessage = ({ message, guessCount, gameOver }) => (
 	<div>
 		<p> {guessCount === 0 ? "" : `Guesses ${guessCount}`} </p>
